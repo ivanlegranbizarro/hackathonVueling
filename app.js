e4a9b670a117620a197a848545ece231602476a6
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-07 18:29:05 +0100

    routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import conexion from "./db/conexion.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import travelRoutes from "./routes/travelRoutes.js";
 
 dotenv.config();
 
@@ -11,10 +12,11 @@ const app = express();
 
 app.use(cors());
 
-
 app.use(express.json());
 
 app.use(express.urlencoded({extended: true}));
+
+app.use("/api", travelRoutes);
 
 
 const PORT = process.env.PORT || 3000;
--- routes/travelRoutes.js
+++ routes/travelRoutes.js
@@ -1,3 +1,18 @@
 import express from "express";
+import TravelControllers from "../controllers/travelControllers.js";
 
+const router = express.Router();
 
+router.get("/airtrip", TravelControllers.getAirtrip);
+router.get("/landtrip", TravelControllers.getLandtrip);
+
+router.put("/airtrip/:id", TravelControllers.updateAirtrip);
+router.put("/landtrip/:id", TravelControllers.updateLandtrip);
+
+router.delete("/airtrip/:id", TravelControllers.deleteAirtrip);
+router.delete("/landtrip/:id", TravelControllers.deleteLandtrip);
+
+router.post("/airtrip", TravelControllers.createAirtrip);
+router.post("/landtrip", TravelControllers.createLandtrip);
+
+export default router;
