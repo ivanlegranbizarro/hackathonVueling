9e91a1a6519a577faad898597e2b1dcafe52663b
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2023-02-07 21:00:50 +0100

    applying validators and finishing!

--- routes/travelRoutes.js
+++ routes/travelRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import TravelControllers from "../controllers/travelControllers.js";
+import {airTripValidation, landTripValidator} from "../validators/travelValidators.js";
+
 
 const router = express.Router();
 
@@ -12,7 +14,7 @@ router.put("/landtrip/:id", TravelControllers.updateLandtrip);
 router.delete("/airtrip/:id", TravelControllers.deleteAirtrip);
 router.delete("/landtrip/:id", TravelControllers.deleteLandtrip);
 
-router.post("/airtrip", TravelControllers.createAirtrip);
-router.post("/landtrip", TravelControllers.createLandtrip);
+router.post("/airtrip", airTripValidation, TravelControllers.createAirtrip);
+router.post("/landtrip", landTripValidator, TravelControllers.createLandtrip);
 
 export default router;
